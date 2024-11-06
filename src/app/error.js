8850c8b8f3f835fe395fa004a504013ee9ade61e
Joseph <joseph.galvin@digisolve.co.uk>
2024-11-06 13:37:55 +0000

    test

--- src/app/error.js
+++ src/app/error.js
@@ -2,7 +2,7 @@
 
 export default function NotFound() {
   return (
-    <div className="h-full w-full flex fixed text-center items-center justify-center text-5xl">
+    <div className="text-5xl pt-40">
       <h2>You are unable to perform this action without signing in.</h2>
     </div>
   );
