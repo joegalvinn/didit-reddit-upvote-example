8f4b160c295d70a672d8d075e66f48690f9829f2
Joseph <joseph.galvin@digisolve.co.uk>
2024-11-06 14:30:16 +0000

    testing

--- src/app/error.js
+++ src/app/error.js
@@ -4,7 +4,7 @@ export default function error() {
   return (
     <div className="text-5xl pt-40">
       <h2>You are unable to perform this action without signing in.</h2>
-      <button onClick={() => reset()}>Go Back</button>
+      <button onClick={() => reset()}>Go Back and try again</button>
     </div>
   );
 }
